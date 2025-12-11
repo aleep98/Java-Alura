402aab6655034987c4c7a7b906e8a61629b9a595
aleep98 <zero167@outlook.com>
2025-12-11 14:07:53 -0300

    feat: adicionar contagem de palavras por linha na leitura de arquivo

--- src/LeitorTxt.java
+++ src/LeitorTxt.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 
 public class LeitorTxt {
+
     public static void main(String[] args) {
         Path caminho = Path.of("C:\\Users\\lalex\\Desktop\\java\\Java-Alura\\src\\entrada.txt");
         try {
             List<String> linhas = Files.readAllLines(caminho);
             for (int i = 0; i < linhas.size(); i++){
                 System.out.println((i+1) + " - " + linhas.get(i));
+                String[] palavras = linhas.get(i).split("\\s+" );
+                System.out.println("Quantidade de palavras na linha " + (i+1) + ": " + palavras.length);
             }
             } catch (IOException e) {
             System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
