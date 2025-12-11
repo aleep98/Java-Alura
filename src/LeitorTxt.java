1dfb5e734eaa50461d198aa30696a669e9590a8d
aleep98 <zero167@outlook.com>
2025-12-11 13:21:08 -0300

    feat: adicionar classe LeitorTxt para leitura de arquivos de texto

---
+++ src/LeitorTxt.java
@@ -0,0 +1,11 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.List;
+
+
+public class LeitorTxt {
+    public static void main(String[] args) {
+        
+    }
+}
