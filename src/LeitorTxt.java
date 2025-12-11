c6856bd53b6e6a054aaa580fa21aff2ae829ba2c
aleep98 <zero167@outlook.com>
2025-12-11 13:41:20 -0300

    feat: implementar leitura de arquivo de texto e exibir linhas

--- src/LeitorTxt.java
+++ src/LeitorTxt.java
@@ -1,11 +1,19 @@
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.io.IOException;
 import java.util.List;
 
 
 public class LeitorTxt {
     public static void main(String[] args) {
-        
+        Path caminho = Path.of("C:\\Users\\lalex\\Desktop\\java\\Java-Alura\\src\\entrada.txt");
+        try {
+            List<String> linhas = Files.readAllLines(caminho);
+            for (int i = 0; i < linhas.size(); i++){
+                System.out.println((i+1) + " - " + linhas.get(i));
+            }
+            } catch (IOException e) {
+            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
     }
+}
 }
